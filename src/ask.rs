4a13473ad97510350f74ef54bcb2a9b1db92dce3
Ian Chen <ianre657@gmail.com>
2022-04-18 21:17:14 +0800

    inquire config

--- src/ask.rs
+++ src/ask.rs
@@ -3,9 +3,40 @@ use inquire::{
     Text,
 };
 
-fn string_ask(prompt: &str, default: &str) -> String {
-    let answer = Text::new(prompt).with_default(default).prompt().unwrap();
-    return answer;
+use crate::config::ConnectConfig;
+
+fn inquire_config(default: &ConnectConfig) -> ConnectConfig {
+    fn ask(prompt: &str, default: &str) -> String {
+        let answer = Text::new(prompt).with_default(default).prompt().unwrap();
+        return answer;
+    }
+
+    let name = ask("Connection name", &default.name);
+    let desc = ask("Description", &default.desc);
+    let user = ask("User", &default.user);
+
+    let server_addr = ask("Server address", &default.server_addr);
+
+
+    // TODO: validation
+    let port_str = ask("Port", &default.port.to_string());
+    let port = port_str.parse::<u32>().unwrap();
+    
+    // TODO: validate input
+    let auth = ask("Authentication method", &default.auth);
+    
+    // TODO: skip if using password
+    let pem_path = ask("Pem path", &default.pem_path);
+
+    return ConnectConfig {
+        name: name,
+        desc: desc,
+        user: user,
+        server_addr: server_addr,
+        port: port,
+        pem_path: pem_path,
+        auth: auth,
+    };
 }
 
 pub fn init() {
@@ -13,18 +44,22 @@ pub fn init() {
 }
 
 pub fn do_ask() {
-    let name = string_ask("Connection name", "my custom connection");
-    let desc = string_ask("Description", "my custom description");
-    let user = string_ask("User", "root");
+    let default_config = ConnectConfig {
+        name: "my custom connection".to_string(),
+        desc: "my custom description".to_string(),
+        user: "root".to_string(),
+        server_addr: "192.168.1.1".to_string(),
+        port: 22,
+        pem_path: "~/.ssh/good".to_string(),
+        auth: "pem".to_string(),
+    };
 
-    println!("name -> {}", name);
-    println!("desc -> {}", desc);
-    println!("user -> {}", user);
+    let final_config = inquire_config(&default_config);
+    println!("final config: {:?}", final_config);
 }
 
 fn get_render_config() -> RenderConfig {
     let mut render_config = RenderConfig::default();
-
     render_config.default_value = StyleSheet::new().with_fg(Color::DarkGrey);
 
     render_config
