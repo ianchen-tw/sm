1e56c150b964a06fe01a84cc6e28718f3d8340bc
Ian Chen <ianre657@gmail.com>
2023-11-11 23:42:32 +0800

    basic file explorer

---
+++ components/sm-file-select/src/main.rs
@@ -0,0 +1,47 @@
+
+use std::{
+    path::{Path},
+};
+
+mod suggest;
+
+use suggest::{OsFileLister, PathSuggester};
+
+
+fn try_list(root: &str, relative_path: &str) {
+    let mut sg = PathSuggester::new(root, &OsFileLister);
+
+    use std::path::Component;
+
+    for comp in Path::new(relative_path).components(){
+        if let Component::Normal(part) = comp{
+            sg.push_path(part.to_str().unwrap());
+        }
+    }
+
+    match sg.suggest_with_strategy_all_nodes() {
+        Ok(res) => {
+            print!("suggest under `{}`: [", sg.current_path().display());
+            let total = 3;
+            for (i, pick) in res.iter().take(total).enumerate() {
+                print!("{}, ", pick);
+                if i == total-1{
+                    print!("...")
+                }
+            }
+            println!("]");
+        }
+        Err(err) => {
+            println!("err: {}", err)
+        }
+    }
+}
+
+fn main() {
+    println!("select file");
+
+    try_list("/home/ian", "");
+    try_list("/home/ian", "./go");
+    //     try_list("/", "home/ian/g");
+    //     try_list("~", ".");
+}
---
+++ components/sm-file-select/src/suggest.rs
@@ -0,0 +1,126 @@
+use anyhow::{Result};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+pub struct PathSuggester<'a, T: ListDir> {
+    root: PathBuf,
+    parents: PathBuf,
+    lister: &'a T,
+}
+
+impl<'a, T: ListDir> PathSuggester<'a, T> {
+    pub fn new(root: &str, lister: &'a T) -> PathSuggester<'a, T> {
+        Self {
+            root: root.into(),
+            parents: PathBuf::new(),
+            lister,
+        }
+    }
+
+    pub fn suggest_with_strategy_all_nodes(&self) -> Result<Vec<String>> {
+        // println!("Listing files in path: {}", &self.get_path().display());
+        self.lister.list_filenames(&self.current_path())
+    }
+
+    pub fn current_path(&self) -> PathBuf {
+        self.root.join(&self.parents)
+    }
+
+    pub fn pop_path(&mut self) -> bool {
+        self.parents.pop()
+    }
+
+    pub fn push_path(&mut self, dir: &str) {
+        self.parents.push(dir);
+    }
+
+}
+
+
+pub trait ListDir {
+    fn new() -> Self;
+    fn list_filenames(&self, dir: &Path) -> Result<Vec<String>>;
+}
+
+pub struct OsFileLister;
+
+impl ListDir for OsFileLister {
+    fn new() -> Self {
+        OsFileLister {}
+    }
+    fn list_filenames(&self, dir: &Path) -> Result<Vec<String>> {
+        let mut result = vec![];
+
+        let entries = fs::read_dir(dir)?;
+        for entry in entries {
+            let entry = entry?;
+            if let Some(name) = entry.path().file_name() {
+                result.push(name.to_string_lossy().into_owned());
+            }
+        }
+        Ok(result)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+    use std::path::Path;
+    use std::vec;
+
+    use anyhow::{Ok, Result};
+
+    use super::{ListDir, PathSuggester};
+    struct FakeLister {
+        payload: RefCell<Vec<String>>,
+    }
+
+    impl FakeLister {
+        fn set_payload(&self, res: Vec<String>) {
+            self.payload.borrow_mut().clone_from(&res);
+        }
+    }
+    impl ListDir for FakeLister {
+        fn new() -> Self {
+            Self {
+                payload: vec![].into(),
+            }
+        }
+        fn list_filenames(&self, _dir: &Path) -> Result<Vec<String>> {
+            Ok(self.payload.borrow().clone())
+        }
+    }
+
+    #[test]
+    fn do_pushpop() {
+        let lister = FakeLister::new();
+
+        let mut sg = PathSuggester::new("/1", &lister);
+        sg.push_path("2");
+        assert_eq!(sg.current_path(), Path::new("/1/2"));
+        sg.pop_path();
+        assert_eq!(sg.current_path(), Path::new("/1"));
+        sg.pop_path();
+        assert_eq!(sg.current_path(), Path::new("/1"));
+
+        lister.set_payload(vec!["a".to_string(), "b".to_string(), "c".to_string()]);
+        sg.current_path();
+    }
+
+    #[test]
+    fn do_suggest() {
+        let lister = FakeLister {
+            payload: vec![].into(),
+        };
+        let sg = PathSuggester::new("/1", &lister);
+
+        lister.set_payload(vec!["a".to_string(), "b".to_string(), "c".to_string()]);
+        assert_eq!(
+            sg.suggest_with_strategy_all_nodes().unwrap(),
+            vec!["a".to_string(), "b".to_string(), "c".to_string()]
+        );
+    }
+}
